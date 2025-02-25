c691a20b4b4c7ecfd9eeb463a0b5f296242bac9a
Lea Petrela <lea.petrela@baruchmail.cuny.edu>
2025-02-24 19:20:30 -0500

    Lea changes 1

--- Animal.py
+++ Animal.py
@@ -1,8 +1,17 @@
 class Animal:
-    def __init__(self, name):
+    def __init__(self, name, skin, run):
         self.__name = name 
+        self.__skin = skin
+        self.__run = run
         print("hello, I am", self.__name)
 
     def talk(self):
-        print("hi")
+        print(f"I talk like a {self.__name}")
+
+    def color(self):
+        print(f"My skin color is {self.__skin}")
+
+    def run(self):
+        print(f"I run very {self.__run}")
+        
 
