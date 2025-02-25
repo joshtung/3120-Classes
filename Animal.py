47151f58c3b404a4bf28fd229d9cc5631f1b82a6
joshtung <josh.tung14@gmail.com>
2025-02-24 20:09:02 -0500

    Revert "Lea changes 1"

--- Animal.py
+++ Animal.py
@@ -1,19 +1,12 @@
 class Animal:
-    def __init__(self, name, skin, run):
-        self.__name = name 
-        self.__skin = skin
-        self.__run = run
+    def __init__(self, name, stamina = 50, hunger = 50): 
+        self.__name = name
+        self.__stamina = stamina
+        self.__hunger = hunger
         print("hello, I am", self.__name)
 
     def talk(self):
-        print(f"I talk like a {self.__name}")
-
-    def color(self):
-        print(f"My skin color is {self.__skin}")
-
-    def run(self):
-        print(f"I run very {self.__run}")
-        
+        print("hi")
 
     def run(self):
         self.__stamina -= 10
